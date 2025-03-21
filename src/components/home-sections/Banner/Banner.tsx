7a30aeac00b377237a0f49f478be6d8062dd5017
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 06:10:30 +0700

    Update Banner.tsx

--- src/components/home-sections/Banner/Banner.tsx
+++ src/components/home-sections/Banner/Banner.tsx
@@ -9,15 +9,13 @@ function Banner() {
         <div className="">
             {/* Phần banner-big và slider */}
             <div className="relative flex-1 flex justify-center items-center">
-                {/* Banner-big với z-index cao hơn */}
                 <img src={banner_big.src} alt="banner" className="object-contain w-full" />
-
-                {/* Slider với z-index thấp hơn và đặt ở phía dưới banner-big */}
-                <div className="w-[100%] absolute top-0 left-1/2 transform -translate-x-1/2">
-                    <BannerSlider images={images} visibleNumber={2} />
-                </div>
             </div>
-
+            
+            <div className="w-[100%] absolute top-0 left-1/2 transform -translate-x-1/2">
+                    <BannerSlider images={images} visibleNumber={2} />
+            </div>
+            
             {/* Phần banner content */}
             <div className="mt-28 gap-3 grid grid-cols-4 max-xl:grid max-lg:grid-cols-2 max-lg:mt-10">
                 {bannerContent.map((value) => (
