394224631072f1cb1d47424ac459f6a8fd579324
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 05:47:21 +0700

    Update BannerSlider.tsx

--- src/components/BannerSlider.tsx
+++ src/components/BannerSlider.tsx
@@ -1,24 +1,33 @@
-'use client'
+'use client';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const BannerSlider = ({images, visibleNumber}) => {
-  return (
-    <Swiper
-    modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={10}
-      slidesPerView={visibleNumber}
-      navigation
-      pagination={{ clickable: true }}
-    >
-        {images.map(((image,index)=><SwiperSlide key={index}><img src={image.src} alt='image' className='rounded-md w-full' width={595} height={180}/></SwiperSlide>))}
-       
-    </Swiper>
-  );
+const BannerSlider = ({ images, visibleNumber }) => {
+    return (
+        <div className="w-full h-[180px] overflow-hidden"> {/* Đặt kích thước cố định cho container */}
+            <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={10}
+                slidesPerView={visibleNumber}
+                navigation
+                pagination={{ clickable: true }}
+                style={{ width: '100%', height: '100%' }} // Đặt kích thước cố định cho Swiper
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <img
+                            src={image.src}
+                            alt="image"
+                            className="rounded-md w-full h-full object-cover" // Đảm bảo hình ảnh phủ đầy slide
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
 };
 
 export default BannerSlider;
-
