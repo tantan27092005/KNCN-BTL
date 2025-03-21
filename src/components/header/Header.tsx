214df5a158ccbbc9ad3fdde1b7c66d5bc8af10e8
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 06:19:51 +0700

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,27 +1,33 @@
-'use client'
+'use client';
 import { menuBar } from './data.mocks';
 import Nav from './Nav';
 import MenuBar from './MenuBar';
-import banner from './assets/banner.png'
+import banner from './assets/banner.png';
 import NavAdmin from '../auth/NavAdmin';
 import { useState } from 'react';
 
-
 function Header() {
-    const [dataPlaces, setDataPlaces] = useState([])
+    const [dataPlaces, setDataPlaces] = useState([]);
+
     return (
-        
         <div className="">
+            {/* Phần NavAdmin (ẩn trên mobile) */}
             <div className='max-xl:hidden'>
-           <NavAdmin/>
+                <NavAdmin />
             </div>
+
+            {/* Phần banner */}
             <header className="w-full">
                 <img src={banner.src} alt="banner" className="w-full" />
             </header>
-            <nav className="padding bg-[#2A83E9] w-full max-xl:bg-transparent">
-                <Nav dataPlaces= {dataPlaces}/>
+
+            {/* Phần Nav cố định khi cuộn trang */}
+            <nav className="padding bg-[#2A83E9] w-full max-xl:bg-transparent sticky top-0 z-50">
+                <Nav dataPlaces={dataPlaces} />
             </nav>
-            <nav className="padding bg-[#2A83E9] w-full max-xl:bg-transparent">
+
+            {/* Phần MenuBar cố định khi cuộn trang */}
+            <nav className="padding bg-[#2A83E9] w-full max-xl:bg-transparent sticky top-[60px] z-40">
                 <MenuBar menuBar={menuBar} />
             </nav>
         </div>
