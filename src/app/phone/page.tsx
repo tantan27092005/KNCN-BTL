131ed3a9d6974e500d3b2a5a3b54b4211207aefc
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 04:03:19 +0700

    Update page.tsx

--- src/app/phone/page.tsx
+++ src/app/phone/page.tsx
@@ -3,7 +3,26 @@ import { fetchPhoneProducts } from '@/api';
 import ProductsPage from '@/components/products-page';
 import { filterDataMobile } from '@/components/products-page/products-data.mocks';
 
-const PhonePage = async ({ searchParams }) => {
+interface SearchParams {
+    category?: string;
+    brand?: string;
+    ram?: number;
+    type?: string;
+    screen?: number;
+    storage?: number;
+    charger?: number;
+    price?: number;
+    pricerange?: number;
+    page?: number;
+    limit?: number;
+    keyword?: string;
+}
+
+interface PhonePageProps {
+    searchParams?: SearchParams;
+}
+
+const PhonePage = async ({ searchParams }: PhonePageProps) => {
     const products = await fetchPhoneProducts({ searchParams });
     return (
         <div>
