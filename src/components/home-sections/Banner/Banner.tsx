3ea18b83f5925fd9f9ad480c3e421b7928177816
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 06:16:50 +0700

    Update Banner.tsx

--- src/components/home-sections/Banner/Banner.tsx
+++ src/components/home-sections/Banner/Banner.tsx
@@ -1,19 +1,19 @@
 import { bannerContent, images } from '../data-mock';
 import BannerContent from './BannerContent';
-import BannerSlider from '@/components/BannerSlider';
+//import BannerSlider from '@/components/BannerSlider';
 import banner_footer from '../assets/banner-footer.png';
 import banner_big from '../assets/banner-big.png';
 
 function Banner() {
     return (
         <div className="">
+            {/* Phần banner-big */}
             <div className="relative flex-1 flex justify-center items-center">
-                {/* Banner-big với z-index cao hơn */}
-                <img src={banner_big.src} alt="banner" className="object-contain w-full z-20" />
+                <img src={banner_big.src} alt="banner" className="object-contain w-full" />
             </div>
 
             {/* Phần banner content */}
-            <div className="mt-28 gap-3 grid grid-cols-4 max-xl:grid max-lg:grid-cols-2 max-lg:mt-10">
+            <div className="mt-10 gap-3 grid grid-cols-4 max-xl:grid max-lg:grid-cols-2 max-lg:mt-6"> {/* Giảm margin-top */}
                 {bannerContent.map((value) => (
                     <BannerContent image={value.image} title={value.title} key={value.title} />
                 ))}
@@ -21,7 +21,7 @@ function Banner() {
 
             {/* Phần banner footer */}
             <div className="w-full">
-                <img src={banner_footer.src} alt="banner" className="w-full my-10 max-lg:my-6 rounded-md" />
+                <img src={banner_footer.src} alt="banner" className="w-full my-6 max-lg:my-4 rounded-md" /> {/* Giảm margin */}
             </div>
         </div>
     );
