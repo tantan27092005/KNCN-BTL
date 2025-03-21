a2629fe4bb8a33ed6113f3e58edb271e0877797b
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 05:51:51 +0700

    Update BannerSlider.tsx

--- src/components/BannerSlider.tsx
+++ src/components/BannerSlider.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
@@ -6,28 +6,28 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 const BannerSlider = ({ images, visibleNumber }) => {
-    return (
-        <div className="w-full h-[180px] overflow-hidden"> {/* Container với kích thước cố định */}
-            <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                spaceBetween={10}
-                slidesPerView={visibleNumber}
-                navigation
-                pagination={{ clickable: true }}
-                style={{ width: '100%', height: '100%' }} // Đặt kích thước cố định cho Swiper
-            >
-                {images.map((image, index) => (
-                    <SwiperSlide key={index}>
-                        <img
-                            src={image.src}
-                            alt="image"
-                            className="rounded-md w-full h-full object-cover" // Đảm bảo hình ảnh phủ đầy slide
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    );
+  return (
+    <div className="w-full h-[180px]"> {/* Thêm container với kích thước cố định */}
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={10}
+        slidesPerView={visibleNumber}
+        navigation
+        pagination={{ clickable: true }}
+        style={{ width: '100%', height: '100%' }} {/* Đặt kích thước cố định cho Swiper */}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={image.src}
+              alt='image'
+              className='rounded-md w-full h-full object-cover' {/* Đảm bảo hình ảnh phủ đầy slide */}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default BannerSlider;
