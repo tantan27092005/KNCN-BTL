d2ff87d8d715a4c695fa51ec134073363466b26b
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 08:41:44 +0700

    Update Button.tsx

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,9 @@
-function Button({ title, color, type, bgBtn, ...props }) {
+function Button({ title, color, type, bgBtn, className, ...props }) {
     const buttonType = type || 'button';
     return (
         <button
             type={buttonType}
-            className={`py-1.5 px-2 border-2 border-${color}-500 rounded-md text-sm max-lg:text-xs cursor-pointer bg-[#f6f7f8] text-${color}-500 hover:text-white active:text-white ${bgBtn}`}
+            className={`py-1.5 px-3 rounded-md text-sm max-lg:text-xs cursor-pointer ${bgBtn} ${className}`}
             {...props}
         >
             {title}
