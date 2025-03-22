982b5747b364aaf27f2442f2705449d189ca1d1b
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 07:23:49 +0700

    Update BannerContent.tsx

--- src/components/home-sections/Banner/BannerContent.tsx
+++ src/components/home-sections/Banner/BannerContent.tsx
@@ -1,7 +1,7 @@
 const BannerContent = ({ image, title }) => {
     return (
         <div className="flex flex-col items-center rounded-2xl p-2 space-y-2">
-            <img src={image.src} className="object-contain w-13.3 h-20 max-lg:w-12 max-lg:h-12" />
+            <img src={image.src} className="object-contain w-10 h-20 max-lg:w-8 max-lg:h-12" />
             <p className="text-xl text-black text-center max-lg:text-base max-lg:font-normal"> 
                 {title}
             </p>
