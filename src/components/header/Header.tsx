8ddf1ef9950d37550c800086bdd3099d932a7a5c
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 07:58:23 +0700

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,28 +1,32 @@
-'use client'
+'use client';
 import { menuBar } from './data.mocks';
 import Nav from './Nav';
 import MenuBar from './MenuBar';
-import banner from './assets/banner.png'
+import banner from './assets/banner.png';
 import NavAdmin from '../auth/NavAdmin';
 import { useState } from 'react';
-
+import LoginButton from './LoginButton'; // Import component LoginButton
 
 function Header() {
-    const [dataPlaces, setDataPlaces] = useState([])
+    const [dataPlaces, setDataPlaces] = useState([]);
+
     return (
-        
         <div className="">
             <div className='max-xl:hidden'>
-           <NavAdmin/>
+                <NavAdmin />
             </div>
             <header className="w-full">
                 <img src={banner.src} alt="banner" className="w-full" />
             </header>
             <nav className="padding bg-[#2A83E9] w-full max-xl:bg-transparent">
-                <Nav dataPlaces= {dataPlaces}/>
+                <Nav dataPlaces={dataPlaces} />
             </nav>
             <nav className="padding bg-[#2A83E9] w-full max-xl:bg-transparent">
                 <MenuBar menuBar={menuBar} />
+                {/* Thay thế "Tài khoản & Đơn hàng" bằng nút đăng nhập */}
+                <div className="flex items-center justify-end pr-4">
+                    <LoginButton />
+                </div>
             </nav>
         </div>
     );
