3071dd2f9f56dc98af6a91a39458b7064a2a1d99
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 09:01:01 +0700

    Update footer.mock.ts

--- src/components/footer/footer.mock.ts
+++ src/components/footer/footer.mock.ts
@@ -1,12 +1,41 @@
-
-export const footerData = [
-    'Tích điểm Quà tặng VIP',
-    'Lịch sử mua hàng',
-    'Tìm hiểu về mua trả góp',
-    'Chính sách bảo hành',
-    'Giới thiệu công ty (MWG.vn)',
-    'Tuyển dụng',
-    'Gửi góp ý, khiếu nại',
-    'Tìm siêu thị (3.043 shop)',
-    'Tổng đài hỗ trợ',
-];
+export const footerData = {
+    contact: {
+        title: 'Tổng đài hỗ trợ',
+        items: [
+            'Mua hàng: 1800.1060 (7:30 - 22:00)',
+            'Kỹ thuật: 1800.1763 (7:30 - 22:00)',
+            'Khiếu nại: 1800.1062 (8:00 - 21:30)',
+            'Bảo hành: 1800.1064 (8:00 - 21:00)',
+        ],
+    },
+    support: {
+        title: 'Hỗ trợ khách hàng',
+        items: [
+            'Mua hàng trả góp',
+            'Hướng dẫn đặt hàng',
+            'Giao hàng & Thanh toán',
+            'Chính sách bảo hành',
+            'Chính sách đổi trả',
+            'Câu hỏi thường gặp',
+        ],
+    },
+    about: {
+        title: 'Về công ty',
+        items: [
+            'Giới thiệu công ty',
+            'Tuyển dụng',
+            'Gửi góp ý, khiếu nại',
+            'Tìm siêu thị (3.043 shop)',
+            'Xem bản mobile',
+        ],
+    },
+    social: {
+        title: 'Kết nối với chúng tôi',
+        items: [
+            'Facebook',
+            'Youtube',
+            'Zalo',
+            'Tiktok',
+        ],
+    },
+};
