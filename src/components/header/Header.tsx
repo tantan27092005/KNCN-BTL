103b8014f68ef3a32b11e55f46f152ca6a304c37
tantan27092005 <162731831+tantan27092005@users.noreply.github.com>
2025-03-22 07:41:41 +0700

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,40 @@
-'use client'
+'use client';
 import { menuBar } from './data.mocks';
 import Nav from './Nav';
 import MenuBar from './MenuBar';
-import banner from './assets/banner.png'
+import banner from './assets/banner.png';
 import NavAdmin from '../auth/NavAdmin';
 import { useState } from 'react';
 
-
 function Header() {
-    const [dataPlaces, setDataPlaces] = useState([])
+    const [dataPlaces, setDataPlaces] = useState([]);
+
     return (
-        
         <div className="">
-            <div className='max-xl:hidden'>
-           <NavAdmin/>
+            {/* Cố định NavAdmin ở phía trên cùng */}
+            <div className='max-xl:hidden fixed top-0 left-0 w-full z-50'>
+                <NavAdmin />
             </div>
-            <header className="w-full">
+
+            {/* Header với banner */}
+            <header className="w-full pt-16"> {/* Thêm padding-top để tránh bị che bởi NavAdmin */}
                 <img src={banner.src} alt="banner" className="w-full" />
             </header>
-            <nav className="padding bg-[#2A83E9] w-full max-xl:bg-transparent">
-                <Nav dataPlaces= {dataPlaces}/>
+
+            {/* Cố định thanh địa điểm (data place) ở phía trên cùng */}
+            <nav className="fixed top-16 left-0 w-full bg-[#2A83E9] z-40 max-xl:bg-transparent">
+                <Nav dataPlaces={dataPlaces} />
             </nav>
-            <nav className="padding bg-[#2A83E9] w-full max-xl:bg-transparent">
+
+            {/* Cố định thanh menu (menu bar) ở phía trên cùng */}
+            <nav className="fixed top-28 left-0 w-full bg-[#2A83E9] z-30 max-xl:bg-transparent">
                 <MenuBar menuBar={menuBar} />
             </nav>
+
+            {/* Phần nội dung chính của trang */}
+            <main className="pt-48"> {/* Thêm padding-top để tránh bị che bởi các thanh cố định */}
+                {/* Nội dung chính của trang */}
+            </main>
         </div>
     );
 }
